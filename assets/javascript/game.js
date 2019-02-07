5a1315ba81c28651147a9b3dcc8f45f4b471ca1e
Peggy Liu <peggyliu@Peggys-MacBook-Air-3.local>
2019-02-07 11:40:52 -0500

    initial site files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,7 @@ var choice = Math.floor(Math.random()*10);
 var answer = game[choice];
 var myLength = answer.length
 var display = [myLenght];
-war win = myLength;
+var win = myLength;
 var letters = answer.split("");
 var attemptsLeft = 10;
 var output = "",
@@ -25,9 +25,49 @@ var setup = function() {
 
 }
 
-    
-"<p>Wins: " + wins + "</p>";
+var submit = function() {
 
-        
+    output = " ";
+    userLetter = $("letters").value;
+    $("letters").value = " ";
+
+
+    for (var c=0; c < answer.length; c++) {
+        alert(letters[c]);
+        if(userLetter.toUpperCase() == letters[c]) {
+            display[c] = userLetter.toUpperCase();
+            win--;
+        }
+            output = output + display[c] + " ";
+
+    }
+
+    document.getElementById("game").innerHTML = output;
+    output = " ";
+    attemptsLeft--;
+
+    if(win < 1) {
+        document.getElementById("guesses").innerHTML = "You win!";
+    }
+
+    else if (attemptsLeft < 1) {
+        document.getElementById("guesses").innerHTML = "You lose!";
+    }
+
+    else {
+        document.getElementById("guesses").innerHTML = "Number of Guesses Remaining: "
+    }
+
+}
+
+
+window.onload = function() {
+    setup();
+    $("submit").onclick = submit;
+}
+    
+"<p>Wins: " + win + "</p>";
+
+"<p>Number of Guesses Remaining: " + attemptsLeft + "</p>";         
         
     
