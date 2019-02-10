9b6663c1880dad368cd8b6a2a4cbca62b264daf0
Peggy Liu <peggyliu@Peggys-MacBook-Air-3.local>
2019-02-09 22:45:26 -0500

    Initial Site Files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -34,58 +34,47 @@ function Game() {
 }
 
 
-var  = document.getElementById("pinocchio");
-var  = document.getElementById("dumbo");
-var  = document.getElementById("bambi");
-var  = document.getElementById("cinderella");
-var  = document.getElementById("aladdin");
-var  = document.getElementById("pocahontas");
-var  = document.getElementById("hercules");
-var  = document.getElementById("mulan");
-var  = document.getElementById("tarzan");
-var  = document.getElementById("frozen");
-
 
 function img() {
     
     if (randomWord === movies[0]) {
-        document.getElementById("pinocchio").src = "./assets/images/pinocchio.png";
+        document.getElementById("image").src = "./assets/images/pinocchio.png";
     }
     
     else if (randomWord === movies[1]) {
-        document.getElementById("dumbo").src="./assets/images/dumbo.png";
+        document.getElementById("image").src="./assets/images/dumbo.png";
     }
     
     else if (randomWord === movies[2]) {
-        document.getElementById("bambi").src="./assets/images/bambi.png";
+        document.getElementById("image").src="./assets/images/bambi.png";
     }
     
     else if (randomWord === movies[3]) {
-        document.getElementById("cinderella").src="./assets/images/cinderella.png";
+        document.getElementById("image").src="./assets/images/cinderella.png";
     }
     
     else if (randomWord === movies[4]) {
-        document.getElementById("aladdin").src="./assets/images/aladdin.jpg";
+        document.getElementById("image").src="./assets/images/aladdin.jpg";
     }
     
     else if (randomWord === movies[5]) {
-        document.getElementById("pocahontas").src="./assets/images/pocahontas.png";
+        document.getElementById("image").src="./assets/images/pocahontas.png";
     }
     
     else if (randomWord === movies[6]) {
-        document.getElementById("hercules").src="./assets/images/hercules.png";
+        document.getElementById("image").src="./assets/images/hercules.png";
     }
 
     else if (randomWord === movies[7]) {
-        document.getElementById("mulan").src="./assets/images/mulan.png";
+        document.getElementById("image").src="./assets/images/mulan.png";
     }
 
     else if (randomWord === movies[8]) {
-        document.getElementById("tarzan").src="./assets/images/tarzan.png";
+        document.getElementById("image").src="./assets/images/tarzan.png";
     }    
 
     else if (randomWord === movies[9]) {
-        document.getElementById("frozen").src="./assets/images/frozen.png";
+        document.getElementById("image").src="./assets/images/frozen.png";
     }
 
 };
@@ -134,9 +123,10 @@ function complete() {
     
     if (lettersOfWord.toString() == blanksAndCorrect.toString()) {
         wins++;
-        reset()
+        img ()
         document.getElementById("winscounter").innerHTML = " " + wins;
-
+        reset()
+        
     } else if (guessesRemaining === 0) {
         losses++;
         reset()
